9089dc6af8124c30b26e5d6c351913356cb91fb8
ordinary22 <ygk7718@daum.net>
2023-08-01 16:41:09 +0900

    boot 프로젝트 생성

---
+++ src/main/java/com/toyProject/matZip/MatZipApplication.java
@@ -0,0 +1,13 @@
+package com.toyProject.matZip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatZipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatZipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyProject/matZip/MatZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyProject.matZip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
