248ca41653c20b245ee7943e70b944905664ec04
ordinary22 <ygk7718@daum.net>
2023-08-01 17:29:24 +0900

    react 연동하기 작업 완료

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 맛집 검색을 위한 프로젝트
+# 환경
+<Strong>프레임 워크</Strong> : Spring boot v3.1.2 </br>
+<Strong>Java</Strong> : v17 </br>
+<Strong>DataBase</Strong> : MariaDB </br>
+<Strong>Front</Strong> : react + typescript
+
---
+++ src/main/java/com/toyProject/matZip/Controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.toyProject.matZip.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
